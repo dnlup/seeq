685a652b46d79df06f4dfdf0c19ce5ea2ff14192
Daniele Belardi <dwon.dnl@gmail.com>
2020-10-30 07:20:00 +0100

    change bench size to 16

--- bench.js
+++ bench.js
@@ -4,7 +4,7 @@ const Benchmark = require('benchmark')
 const Seeq = require('./')
 
 const suite = new Benchmark.Suite()
-const size = 256
+const size = 16
 
 suite
   .add('Array.prototype.splice + Array.prototype[Symbol.iterator]', function array () {
@@ -16,7 +16,7 @@ suite
     for (const item of b) {
     }
   })
-  .add('Seeq.prototype[Symbol.iterator]', function sique () {
+  .add('Seeq.prototype[Symbol.iterator]', function seeq () {
     const s = new Seeq(size)
     for (let i = 0; i < size; i++) {
       s.push(i)
